bd074654f5329a82c4f3e823f7e271dc0fb4c869
atillaturkmen <turkmenatilla522@gmail.com>
2021-10-29 13:07:53 +0300

    jam-mobile-6 show warning when phone or server is disconnected

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -3,10 +3,10 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+
 import '/domain/user.dart';
 import '/util/app_url.dart';
 import '/util/shared_preference.dart';
-
 
 enum Status {
   NotLoggedIn,
@@ -19,16 +19,16 @@ enum Status {
 }
 
 class AuthProvider with ChangeNotifier {
-
   Status _loggedInStatus = Status.NotLoggedIn;
   Status _registeredInStatus = Status.NotRegistered;
 
   Status get loggedInStatus => _loggedInStatus;
-  Status get registeredInStatus => _registeredInStatus;
 
+  Status get registeredInStatus => _registeredInStatus;
 
   Future<Map<String, dynamic>> login(String? email, String? password) async {
     var result;
+    var response;
 
     final Map<String, dynamic> loginData = {
       'username': email,
@@ -38,16 +38,25 @@ class AuthProvider with ChangeNotifier {
     _loggedInStatus = Status.Authenticating;
     notifyListeners();
 
-    Response response = await post(
-      Uri.parse(AppUrl.login),
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8'
-      },
-      body: json.encode(loginData),
-    );
+    try {
+      response = await post(
+        Uri.parse(AppUrl.login),
+        headers: {'Content-Type': 'application/json; charset=UTF-8'},
+        body: json.encode(loginData),
+      );
+    } catch (err) {
+      print(err);
+
+      _loggedInStatus = Status.NotLoggedIn;
+      notifyListeners();
+
+      return {
+        'status': false,
+        'message': "Error when connecting server",
+      };
+    }
 
     if (response.statusCode == 200) {
-
       User authUser = User.fromJson(loginData);
 
       UserPreferences().saveUser(authUser);
@@ -59,25 +68,35 @@ class AuthProvider with ChangeNotifier {
     } else {
       _loggedInStatus = Status.NotLoggedIn;
       notifyListeners();
-      result = {
-        'status': false,
-        'message': response.body
-      };
+      result = {'status': false, 'message': response.body};
     }
     return result;
   }
 
-  Future<dynamic> register(String? email, String? password, String? passwordConfirmation) async {
+  Future<dynamic> register(
+      String? email, String? password, String? passwordConfirmation) async {
     var result;
+    var response;
 
     final Map<String, String?> registrationData = {
       'username': email,
       'password': password,
     };
-    Response response = await post(
+
+    try {
+      response = await post(
         Uri.parse(AppUrl.register),
         body: json.encode(registrationData),
-        headers: {'Content-Type': 'application/json'});
+        headers: {'Content-Type': 'application/json'},
+      );
+    } catch (err) {
+      print(err);
+
+      return {
+        'status': false,
+        'message': "Error when connecting server",
+      };
+    }
 
     if (response.statusCode == 200) {
       User authUser = User.fromJson(registrationData);
@@ -89,12 +108,8 @@ class AuthProvider with ChangeNotifier {
         'data': authUser
       };
     } else {
-      result = {
-        'status': false,
-        'message': response.body
-      };
+      result = {'status': false, 'message': response.body};
     }
     return result;
   }
-
 }
