cf28d3c32d7a0420ae0f032b6285cd95f3937c81
atillaturkmen <turkmenatilla522@gmail.com>
2021-12-01 12:24:45 +0300

    change email to username

--- lib/util/shared_preference.dart
+++ lib/util/shared_preference.dart
@@ -23,7 +23,7 @@ class UserPreferences {
 
   void removeUser() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.remove("email");
+    prefs.remove("username");
     prefs.remove("token");
   }
 
