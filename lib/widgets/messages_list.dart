f2ab5ccba88038a12d1bd8737c611614fde0b853
atillaturkmen <turkmenatilla522@gmail.com>
2022-02-02 23:16:51 +0300

    add loading wait for messages to avoid crash on start up

--- lib/widgets/messages_list.dart
+++ lib/widgets/messages_list.dart
@@ -7,78 +7,94 @@ import 'package:jam/util/util_functions.dart';
 import 'package:provider/provider.dart';
 
 messagesList(userId, context) {
-  return ValueListenableBuilder(
-      valueListenable:
-          Hive.box<ChatPair>('${onlyASCII(userId)}:messages').listenable(),
-      builder: (context, Box<ChatPair> box, widget) {
-        List<ChatPair> chats = box.values.toList().cast();
-        chats.sort();
-        if (chats.length == 0) {
-          return Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.messenger_outlined),
-              SizedBox(height: 10),
-              Text("No messages yet"),
-            ],
-          ));
-        }
-        return ListView.separated(
-          shrinkWrap: true,
-          // needed for this scrollable widget inside another scrollable widget
-          itemBuilder: (context, index) => Padding(
-            padding: EdgeInsets.only(top: 5, bottom: 5),
-            child: ListTile(
-              leading: Image(
-                image: AssetImage("assets/avatar.png"),
-              ),
-              title: Text(chats[index].username),
-              subtitle: Text(chats[index].lastMessage),
-              trailing: chats[index].unreadMessages == 0
-                  ? Text("")
-                  : Container(
-                      padding: EdgeInsets.all(1),
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(100),
+  String boxName = '${onlyASCII(userId)}:messages';
+  Future boxOpening = Provider.of<MessageProvider>(context, listen: false)
+      .openBox(boxName);
+
+  return FutureBuilder(
+    future: boxOpening,
+    builder: (context, snapshot) {
+      switch (snapshot.connectionState) {
+        case ConnectionState.none:
+        case ConnectionState.waiting:
+          print("messages future builder waiting");
+          return Center(child: CircularProgressIndicator());
+        default:
+          return ValueListenableBuilder(
+              valueListenable:
+              Hive.box<ChatPair>(boxName).listenable(),
+              builder: (context, Box<ChatPair> box, widget) {
+                List<ChatPair> chats = box.values.toList().cast();
+                chats.sort();
+                if (chats.length == 0) {
+                  return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.messenger_outlined),
+                          SizedBox(height: 10),
+                          Text("No messages yet"),
+                        ],
+                      ));
+                }
+                return ListView.separated(
+                  shrinkWrap: true,
+                  // needed for this scrollable widget inside another scrollable widget
+                  itemBuilder: (context, index) => Padding(
+                    padding: EdgeInsets.only(top: 5, bottom: 5),
+                    child: ListTile(
+                      leading: Image(
+                        image: AssetImage("assets/avatar.png"),
                       ),
-                      constraints: BoxConstraints(
-                        minWidth: 30,
-                        minHeight: 30,
-                      ),
-                      child: Text(
-                        chats[index].unreadMessages.toString(),
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
+                      title: Text(chats[index].username),
+                      subtitle: Text(chats[index].lastMessage),
+                      trailing: chats[index].unreadMessages == 0
+                          ? Text("")
+                          : Container(
+                        padding: EdgeInsets.all(1),
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(100),
                         ),
-                        textAlign: TextAlign.center,
+                        constraints: BoxConstraints(
+                          minWidth: 30,
+                          minHeight: 30,
+                        ),
+                        child: Text(
+                          chats[index].unreadMessages.toString(),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
+                      onTap: () {
+                        var name = chats[index].username;
+                        var id = chats[index].userId;
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DM(
+                                otherUsername: name,
+                                otherId: id,
+                                unRead:
+                                Provider.of<MessageProvider>(context, listen: false)
+                                    .messages
+                                    .get(id)
+                                    .unreadMessages),
+                          ),
+                        );
+                      },
                     ),
-              onTap: () {
-                var name = chats[index].username;
-                var id = chats[index].userId;
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DM(
-                        otherUsername: name,
-                        otherId: id,
-                        unRead:
-                            Provider.of<MessageProvider>(context, listen: false)
-                                .messages
-                                .get(id)
-                                .unreadMessages),
                   ),
+                  separatorBuilder: (context, index) => Divider(
+                    color: Colors.grey,
+                  ),
+                  itemCount: chats.length,
                 );
-              },
-            ),
-          ),
-          separatorBuilder: (context, index) => Divider(
-            color: Colors.grey,
-          ),
-          itemCount: chats.length,
-        );
-      });
+              });
+      }
+    },
+  );
 }
