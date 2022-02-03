27fad333004aeacdbe7d04131b9e6b5b151c1cb6
atillaturkmen <turkmenatilla522@gmail.com>
2022-02-03 10:10:16 +0300

    contact us page ui

--- lib/pages/contact_us.dart
+++ lib/pages/contact_us.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:jam/widgets/form_widgets.dart';
 import 'package:jam/widgets/show_snackbar.dart';
@@ -14,36 +12,36 @@ class ContactUs extends StatelessWidget {
         elevation: 0.1,
       ),
       body: Column(
-      children: [
-        SizedBox(height: 20),
-        Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Text(
-            "Please send us your ideas and suggestions",
-            style: TextStyle(fontSize: 30),
-            textAlign: TextAlign.center,
+        children: [
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              "Please send us your ideas and suggestions",
+              style: TextStyle(fontSize: 30),
+              textAlign: TextAlign.center,
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Container(
-            child: TextField(
-              minLines: 12,
-              maxLines: null,
-              keyboardType: TextInputType.multiline,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-              style: TextStyle(
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              child: TextField(
+                minLines: 12,
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+                style: TextStyle(),
               ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: longButtons("Send", () => {showSnackBar(context, "pressed")}),
-        ),
-      ],
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child:
+                longButtons("Send", () => {showSnackBar(context, "pressed")}),
+          ),
+        ],
       ),
     );
   }
