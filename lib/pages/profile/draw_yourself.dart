a605ffa0bad9718e43d669e443dfce22bf8ff946
atillaturkmen <turkmenatilla522@gmail.com>
2022-04-03 12:41:45 +0300

    added drawable container

--- lib/pages/profile/draw_yourself.dart
+++ lib/pages/profile/draw_yourself.dart
@@ -6,15 +6,55 @@ class DrawYourself extends StatefulWidget {
 }
 
 class _DrawYourselfState extends State<DrawYourself> {
+  List<Offset?> _points = <Offset?>[];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.pinkAccent,
-        title: Text("Draw Your Profile Picture"),
-        elevation: 0.1,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
+        child: GestureDetector(
+          onPanUpdate: (DragUpdateDetails details) {
+            setState(() {
+              RenderBox? referenceBox =
+                  context.findRenderObject() as RenderBox?;
+              Offset? localPosition =
+                  referenceBox?.globalToLocal(details.globalPosition);
+              _points = new List.from(_points)..add(localPosition);
+              print(localPosition);
+            });
+          },
+          onPanEnd: (DragEndDetails details) => _points.add(null),
+          child: new CustomPaint(painter: new SignaturePainter(_points)),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Icon(Icons.check),
       ),
     );
   }
+}
+
+class SignaturePainter extends CustomPainter {
+  SignaturePainter(this.points);
+
+  final List<Offset?> points;
+
+  void paint(Canvas canvas, Size size) {
+    Paint paint = new Paint()
+      ..color = Colors.black
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5.0;
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null)
+        canvas.drawLine(points[i]!, points[i + 1]!, paint);
+    }
+  }
+
+  bool shouldRepaint(SignaturePainter other) => other.points != points;
 }
