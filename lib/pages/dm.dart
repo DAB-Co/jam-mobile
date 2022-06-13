6668b3d3ec17f7127ad11c7c66dd164b4ea53a28
atillaturkmen <turkmenatilla522@gmail.com>
2022-06-13 09:32:59 +0300

    added media selection to dm page

--- lib/pages/dm.dart
+++ lib/pages/dm.dart
@@ -124,7 +124,8 @@ class _DMState extends State<DM> with WidgetsBindingObserver {
     AlertDialog alertDialog = alert(
       "Do you really want to block $otherUsername?",
       blockButton,
-      content: Text("You won't be able to receive messages from $otherUsername."),
+      content:
+          Text("You won't be able to receive messages from $otherUsername."),
     );
 
     void _handleThreeDotClick(String value) {
@@ -333,6 +334,15 @@ class _DMState extends State<DM> with WidgetsBindingObserver {
                       ),
                     ),
                   ),
+                  IconButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                          backgroundColor: Colors.transparent,
+                          context: context,
+                          builder: (builder) => bottomSheet());
+                    },
+                    icon: Icon(Icons.attach_file),
+                  ),
                   SizedBox(
                     width: 15,
                   ),
@@ -354,6 +364,86 @@ class _DMState extends State<DM> with WidgetsBindingObserver {
               ),
             ),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget bottomSheet() {
+    return Container(
+      height: 278,
+      width: MediaQuery.of(context).size.width,
+      child: Card(
+        margin: const EdgeInsets.all(18.0),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  iconCreation(
+                      Icons.insert_drive_file, Colors.indigo, "Document"),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  iconCreation(Icons.camera_alt, Colors.pink, "Camera"),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  iconCreation(Icons.insert_photo, Colors.purple, "Gallery"),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  iconCreation(Icons.headset, Colors.orange, "Audio"),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  iconCreation(Icons.location_pin, Colors.teal, "Location"),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  iconCreation(Icons.person, Colors.blue, "Contact"),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget iconCreation(IconData icons, Color color, String text) {
+    return InkWell(
+      onTap: () {},
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: color,
+            child: Icon(
+              icons,
+              // semanticLabel: "Help",
+              size: 29,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            text,
+            style: TextStyle(
+              fontSize: 12,
+              // fontWeight: FontWeight.w100,
+            ),
+          )
         ],
       ),
     );
